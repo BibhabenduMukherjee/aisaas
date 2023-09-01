d997c724c988e6d10d7daecb7e9c550eb3d6eb57
= <mukherjee4004@gmail.com>
2023-09-01 20:33:31 +0530

    container gcp build 4

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,7 +4,14 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prismadb = globalThis.prisma || new PrismaClient()
+const prismadb = globalThis.prisma || new PrismaClient({
+  datasources : {
+    db : {
+      url: process.env.DATABASE_URL
+    }
+  
+  }
+})
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
 
 export default prismadb;
